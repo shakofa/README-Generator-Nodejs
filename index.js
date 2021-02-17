986f260ef9ca64f409596e494452f8124a6003b3
Anahita <shakofa.mahmody@gmail.com>
2021-02-17 01:43:50 -0800

    Add initialize functions and to write the readme file funtion.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const path = require('path');
-const generateMarkdown = require('./utils/generateMarkdown')
 
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
      
 // array of questions for user
@@ -10,88 +10,80 @@ const generateMarkdown = require('./utils/generateMarkdown')
 
         {
             type: 'input',
-            message = 'What is your GitHub username?',
+            message: 'What is your GitHub username?',
             name: 'github',
-           // validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
+           validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
         },
         {
             type: 'input',
-            message = 'What is your email address?',
+            message: 'What is your email address?',
             name: 'email',
-           // validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
+            validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
         },
     
         {
             type: 'input',
-            message = 'What is the project title?',
+            message: 'What is the project title?',
             name: 'title',
-            //validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
+            validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
         },
         {
             type: 'input',
-            message = 'What command do you use to install dependancies?',
+            message: 'What command do you use to install dependancies?',
             name: 'installation',
             default: 'npm i',
-          //  validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
+             validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
         },
         {
             type: 'input',
-            message = ' Please write a short description about this project',
-            name: 'describtion',
-           // validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
+            message: ' Please write a short description about this project',
+            name: 'description',
+            validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
         },
         {
             type: 'input',
-            message = 'What command should be executed to run tests?',
+            message: 'What command should be executed to run tests?',
             name: 'test',
             default: 'npm test',
-            //validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
+            validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
         },
         {
             type: 'input',
-            message = 'How can the user use your app?',
+            message: 'How can the user use your app?',
             name: 'usage',
-            //validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
+            validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
         },
         {
             type: 'input',
-            message = 'What does the user need to know about contributing to the this repo?',
+            message: 'Who contributed on this project?',
             name: 'contribution',
-            //validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
+            validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
         },
         {
             //list of licenses
             type: 'list',
-            message = 'what kind of license is needed for this project?',
+            message: 'what kind of license is needed for this project?',
             name: 'license',
             choices: ['MIT', 'GPL', 'APACHE', 'GNU', 'None'],
-            //validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
+            validate: (value) =>{ if(value){return true} else {return 'it need a value to continue'}}
         },
         
     ]
-    
-   
+
 // function to write README file
 function writeToFile(fileName, data) {
-
-    return fs.writeToFile(`./${fileName.toLowerCase().split('.').join('')}.md`, data, (err) => {
-        if(err){
-            console.log(err)
-        }
-        console.log("your README has been generated.")
-    })
-}
-
-
-// function to initialize program
-function init() {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  }
+  
+  // function to initialize program
+  function init() {
     inquirer.prompt(questions)
     .then((inquirerResponses) => {
       console.log("Generating README...");
       writeToFile("README.md", generateMarkdown({...inquirerResponses}));
     })
+  }
   
-}
-
-// function call to initialize program
-init();
+  // function call to initialize program
+  init();
+  
